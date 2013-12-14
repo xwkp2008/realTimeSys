a2f5d3485bcec748c798dfefcca5fd440e9ac2d0
hjhfg1004 <hjhfg1004@163.com>
2013-12-14 11:17:22 +0800

    人员配置模块
    列表和增删查改基本完成，如必要后续添加字段和模拟数据。

--- realTimeSys/app/controllers/system/privilege/Staff.java
+++ realTimeSys/app/controllers/system/privilege/Staff.java
@@ -1,12 +1,40 @@
 package controllers.system.privilege;
 
+import java.io.File;
+
+import play.Play;
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.system.privilege.staffConfig;
+import tng.ark.common.util.FileUtils;
+import views.html.list;
+import views.html.system.privilege.staff.*;
 
 public class Staff extends Controller{
 	// 成员列表
 	public static Result list() {
-		return ok(staffConfig.render(""));
+		File jsonFile = Play.application().getFile(
+				"/app/controllers/json/staffList.json");
+		String jsonStr = "{}";
+		try {
+			jsonStr = FileUtils.readFileToString(jsonFile);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ok(list.render(jsonStr));
     }
+	
+	// 详情
+	public static Result detail(){
+		return ok(staffDetail.render(""));
+	}
+	
+	// 添加人员
+	public static Result add(){
+		return ok(staffAdd.render(""));
+	}
+	
+	// 修改人员
+	public static Result edit(){
+		return ok(staffEdit.render(""));
+	}
 }
---
+++ realTimeSys/public/javascripts/staff/staffEdit.js
@@ -0,0 +1,9 @@
+function save(){
+	$.pdialog.closeCurrent();
+	alertMsg.correct("人员新增成功。");
+}
+
+function saveEdit(){
+	$.pdialog.closeCurrent();
+	alertMsg.correct("人员资料修改成功。");
+}
