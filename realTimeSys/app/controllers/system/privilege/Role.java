79c885e3d55954c3633deeefc0cd83d30df56f6a
xieming <423899851@qq.com>
2013-12-14 12:29:12 +0800

    角色管理

---
+++ realTimeSys/app/controllers/system/privilege/Role.java
@@ -0,0 +1,35 @@
+package controllers.system.privilege;
+
+import java.io.File;
+
+import play.Play;
+import play.mvc.Controller;
+import play.mvc.Result;
+import tng.ark.common.util.FileUtils;
+import views.html.list;
+import views.html.system.privilege.role.*;
+
+public class Role extends Controller{
+	// 角色列表
+	public static Result list() {
+		File jsonFile = Play.application().getFile(
+				"/app/controllers/json/roleList.json");
+		String jsonStr = "{}";
+		try {
+			jsonStr = FileUtils.readFileToString(jsonFile);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ok(list.render(jsonStr));
+    }
+	
+	// 添加角色
+	public static Result add(){
+		return ok(roleAdd.render(""));
+	}
+	
+	// 修改角色
+	public static Result edit(){
+		return ok(roleEdit.render(""));
+	}
+}
---
+++ realTimeSys/public/javascripts/role/roleEdit.js
@@ -0,0 +1,9 @@
+function saveRole(){
+	$.pdialog.closeCurrent();
+	alertMsg.correct("角色新增成功。");
+}
+
+function saveRoleEdit(){
+	$.pdialog.closeCurrent();
+	alertMsg.correct("角色修改成功。");
+}
