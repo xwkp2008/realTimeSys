d491eaedaa06095c067601796d7cc404ce35a9e0
hjhfg1004 <hjhfg1004@163.com>
2013-12-17 23:54:37 +0800

    总览 网络实时监控
    基本界面 待完善

--- realTimeSys/app/controllers/monitor/status/RunningStatus.java
+++ realTimeSys/app/controllers/monitor/status/RunningStatus.java
@@ -1,10 +1,33 @@
 package controllers.monitor.status;
 
+import java.io.File;
+
+import play.Play;
 import play.mvc.Controller;
 import play.mvc.Result;
+import tng.ark.common.util.FileUtils;
+import views.html.list;
 import views.html.monitor.status.*;
 
 public class RunningStatus extends Controller {
+	// 总览
+	public static Result overview(){
+		File jsonFile = Play.application().getFile(
+				"/app/controllers/json/overview.json");
+		String jsonStr = "{}";
+		try {
+			jsonStr = FileUtils.readFileToString(jsonFile);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ok(list.render(jsonStr));
+	}
+	
+	// 总览详情
+	public static Result overviewDetail(){
+		return ok(overviewDetail.render(""));
+	}
+	
 	// 内存运行状态一览
 	public static Result memory() {
 		return ok(memory.render(""));
@@ -23,6 +46,11 @@ public class RunningStatus extends Controller {
 	// CPU状态一览
 	public static Result CPU() {
 		return ok(CPU.render(""));
+    }
+	
+	// 网络状态一览
+	public static Result network() {
+		return ok(network.render(""));
     }
 	
 }
