f00800f41f0cc1bba461b7d02868bc816cc09037
xieming <423899851@qq.com>
2013-12-19 22:59:21 +0800

    增加数据库配置参数

--- realTimeSys/app/controllers/monitor/configfile/ConfigFile.java
+++ realTimeSys/app/controllers/monitor/configfile/ConfigFile.java
@@ -7,6 +7,7 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import tng.ark.common.util.FileUtils;
 import views.html.list;
+import views.html.monitor.configfile.dbParamList;
 
 public class ConfigFile extends Controller{
 	// 操作系统参数列表
@@ -21,6 +22,42 @@ public class ConfigFile extends Controller{
 		}
 		return ok(list.render(jsonStr));
     }
+	// 数据库参数主页
+	public static Result dbParamList() {
+		File jsonFile = Play.application().getFile(
+				"/app/controllers/json/OParamList.json");
+		String jsonStr = "{}";
+		try {
+			jsonStr = FileUtils.readFileToString(jsonFile);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ok(dbParamList.render(list.render(jsonStr).toString()));
+    }
 	
+	// ORACLE参数列表
+	public static Result OParamList() {
+		File jsonFile = Play.application().getFile(
+				"/app/controllers/json/OParamList.json");
+		String jsonStr = "{}";
+		try {
+			jsonStr = FileUtils.readFileToString(jsonFile);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ok(list.render(jsonStr));
+    }
 	
+	// informix参数列表
+	public static Result IParamList() {
+		File jsonFile = Play.application().getFile(
+				"/app/controllers/json/IParamList.json");
+		String jsonStr = "{}";
+		try {
+			jsonStr = FileUtils.readFileToString(jsonFile);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ok(list.render(jsonStr));
+    }
 }
