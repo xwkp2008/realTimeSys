de5ca39a718bfce0520b9744b34634672485ff38
xieming <423899851@qq.com>
2013-12-22 14:08:28 +0800

    SQL记录采集

---
+++ realTimeSys/app/controllers/analyse/Analyse.java
@@ -0,0 +1,18 @@
+package controllers.analyse;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.analyse.sqlCollect;
+
+public class Analyse extends Controller {
+	// sql记录采集
+	public static Result sqlCollect() {
+		return ok(sqlCollect.render(""));
+    }
+	
+	// sql记录采集
+	public static Result sqlDetail() {
+		return ok(views.html.analyse.sqlDetail.render(""));
+    }
+	
+}
